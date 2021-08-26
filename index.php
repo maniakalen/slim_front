f2429fdc84f31eb16aaebfe745c609b86d0bd117
maniakalen <pesho10@yahoo.com>
2021-08-26 23:24:04 +0300

    Adding font-awesome for icons and preparing basic admin listing

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-    <title>Hello World</title>
+    <title>Blog</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/all.css" rel="stylesheet" />
 </head>
 <body>
-<div class="col-md-12">
-    <div class="pull-right col-md-4">
-        <a href="#">Login</a>
+<div class="col-md-12 menu">
+    <div class="pull-right col-md-4 public-menu">
+        <a href="#" id="login-button">Login</a> | <a href="#" id="load-posts">Posts</a>
     </div>
 </div>
 <div id="root" class="container"></div>
@@ -17,14 +18,46 @@
 <script src="js/handlebars.js"></script>
 <script src="js/script.js"></script>
 <script id="post-template" type="text/html">
-    <div class="row">
+    {{#each this}}
+    <div class="row justify-content-md-center">
         <div class="col-md-4">
             <img src="http://localhost:8080/img/{{ photo_name }}" height="200" />
         </div>
         <div class="col-md-8">
             <div class="col-md-12">{{ title }}</div>
+            <div class="col-md-12"><sup>{{ updated_at }}</sup></div>
             <div class="col-md-12">{{ content }}</div>
         </div>
+    </div>
+    {{/each}}
+</script>
+<script id="post-admin-template" type="text/html">
+    {{#each this}}
+    <div class="row justify-content-md-center">
+        <div class="col-md-2">{{ id }}</div>
+        <div class="col-md-2">{{ author_id }}</div>
+        <div class="col-md-2">{{ title }}</div>
+        <div class="col-md-2">{{ content }}</div>
+        <div class="col-md-2">{{ photo_name }}</div>
+        <div class="col-md-2"><i class="btn btn-default far fa-edit" data-key="{{ id }}"></i> | <i class="btn btn-default far fa-trash-alt"  data-key="{{ id }}"></i></div>
+    </div>
+    {{/each}}
+</script>
+<script id="login-template" type="text/html">
+    <div class="row justify-content-md-center">
+        <form>
+    <div class="row col-md-4 form-group">
+        <div class="col-md-12">
+            <input id="username" class="form-control" placeholder="Username" />
+        </div>
+        <div class="col-md-12">
+            <input id="password" class="form-control" placeholder="Password" />
+        </div>
+        <div class="col-md-12">
+            <button id="login" class="btn btn-primary">Login</button>
+        </div>
+    </div>
+        </form>
     </div>
 </script>
 </body>
--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
+    let backend = "http://localhost:8080";
+    let token = null;
     let post = Handlebars.compile($('#post-template').html());
-    $.get('http://localhost:8080/posts', function(data) {
-        let container = $('#root');
-        data.forEach(function(item) { container.append(post(item)); })
+    let postAdmin = Handlebars.compile($('#post-admin-template').html());
+    let container = $('#root');
+    let loadPosts = function() {
+        $.get('http://localhost:8080/posts', function (data) {
+            container.html(post(data));
+        });
+    }
+    loadPosts();
+    $('body').on('click', '#load-posts', loadPosts)
+        .on('click', '#login-button', function(e) {
+            let login = Handlebars.compile($('#login-template').html());
+            container.html(login());
+            return false;
+        }).on('click', 'button#login', function(e) {
+        let username = $('#username').val();
+        let password = $('#password').val();
+        $.ajax({
+            "method":"POST",
+            "url" : backend + "/user/login",
+            "data": JSON.stringify({"username":username, "password":password}),
+            "contentType": "application/json"
+        }).done(function(data) {
+            if (data.success && typeof data.body !== "undefined") {
+                token = data.body;
+                loadPostsForEdit();
+            }
+        });
     });
+    let loadPostsForEdit = function() {
+        $.get('http://localhost:8080/posts', function (data) {
+            container.html(postAdmin(data));
+        });
+    };
 });
