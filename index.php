77ed70dd4838a27faedf586061a415c9aa3e9ba0
maniakalen <pesho10@yahoo.com>
2021-08-27 09:25:17 +0300

    Add edit/create/delete functionalities with file upload

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
 <div class="col-md-12 menu">
     <div class="pull-right col-md-4 public-menu">
-        <a href="#" id="login-button">Login</a> | <a href="#" id="load-posts">Posts</a>
+        <a href="#" id="login-button" class="btn btn-link">Login</a> <a href="#" id="logout-button" class="d-none btn btn-link">Logout</a>| <a href="#" id="load-posts" class="btn btn-link">Posts</a>
     </div>
 </div>
 <div id="root" class="container"></div>
@@ -42,15 +42,40 @@
         <div class="col-md-2"><i class="btn btn-default far fa-edit" data-key="{{ id }}"></i> | <i class="btn btn-default far fa-trash-alt"  data-key="{{ id }}"></i></div>
     </div>
     {{/each}}
+    <div class="row justify-content-md-center">
+        <div class="col-md-12"><a href="#" id="add-new" class="btn btn-primary">Add new post</a></div>
+    </div>
+</script>
+<script id="post-edit-template" type="text/html">
+    <div class="row justify-content-md-center">
+        <div class="col-md-12"><a href="#" id="back-to-list">Back to list</a></div>
+        <form id="post_edit">
+            <div class="form-group row mb-3">
+                <div class="col-md-2 "><label class="col-form-label" for="photo_name">Image</label></div>
+                <div class="col-md-10">{{#if photo_name}}<img src="http://localhost:8080/img/{{ photo_name }}" height="100"/>{{/if}}<input type="file" name="photo_name" id="photo_name" class="form-control" /></div>
+            </div>
+            <div class="form-group row mb-3">
+                <div class="col-md-2"><label class="col-form-label" for="title">Title</label></div>
+                <div class="col-md-10"><input type="text" value="{{ title }}" class="form-control" name="title" id="title" /></div>
+            </div>
+            <div class="form-group row mb-3">
+                <div class="col-md-2"><label class="col-form-label" for="content">Content</label></div>
+                <div class="col-md-10"><textarea class="form-control" name="content" id="content">{{ content }}</textarea></div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-12"><button id="edit_post" class="btn btn-primary" data-id="{{ id }}">Submit</button></div>
+            </div>
+        </form>
+    </div>
 </script>
 <script id="login-template" type="text/html">
     <div class="row justify-content-md-center">
         <form>
     <div class="row col-md-4 form-group">
-        <div class="col-md-12">
+        <div class="col-md-12 mb-3">
             <input id="username" class="form-control" placeholder="Username" />
         </div>
-        <div class="col-md-12">
+        <div class="col-md-12 mb-3">
             <input id="password" class="form-control" placeholder="Password" />
         </div>
         <div class="col-md-12">
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     let token = null;
     let post = Handlebars.compile($('#post-template').html());
     let postAdmin = Handlebars.compile($('#post-admin-template').html());
+    let editForm = Handlebars.compile($('#post-edit-template').html());
     let container = $('#root');
     let loadPosts = function() {
         $.get('http://localhost:8080/posts', function (data) {
@@ -10,29 +11,131 @@ $(document).ready(function() {
         });
     }
     loadPosts();
+    let loadPostsForEdit = function() {
+        $.get('http://localhost:8080/posts', function (data) {
+            container.html(postAdmin(data));
+        });
+    };
     $('body').on('click', '#load-posts', loadPosts)
         .on('click', '#login-button', function(e) {
             let login = Handlebars.compile($('#login-template').html());
             container.html(login());
             return false;
         }).on('click', 'button#login', function(e) {
-        let username = $('#username').val();
-        let password = $('#password').val();
-        $.ajax({
-            "method":"POST",
-            "url" : backend + "/user/login",
-            "data": JSON.stringify({"username":username, "password":password}),
-            "contentType": "application/json"
-        }).done(function(data) {
-            if (data.success && typeof data.body !== "undefined") {
-                token = data.body;
-                loadPostsForEdit();
-            }
+            let username = $('#username').val();
+            let password = $('#password').val();
+            $.ajax({
+                "method":"POST",
+                "url" : backend + "/user/login",
+                "data": JSON.stringify({"username":username, "password":password}),
+                "contentType": "application/json"
+            }).done(function(data) {
+                if (data.success && typeof data.body !== "undefined") {
+                    token = data.body;
+                    $('#login-button').addClass('d-none');
+                    $('#logout-button').removeClass('d-none');
+                    loadPostsForEdit();
+                }
+            });
+            return false;
+        }).on('click', '#logout-button', function() {
+            $.ajax({
+                url: backend + '/user/logout',
+                type: 'POST',
+                contentType: "application/json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader ("Authorization", "Bearer " + token);
+                }
+            }).done(function(data) {
+                if (data.success) {
+                    token = null;
+                    $('#logout-button').addClass('d-none');
+                    $('#login-button').removeClass('d-none');
+                    loadPosts();
+                }
+            });
+            return false;
+        }).on('click', 'i.fa-edit', function(e) {
+            let id = $(e.target).data('key');
+            $.get(backend + "/post/" + id).done(function(data) {
+                if (data.success) {
+                    container.html(editForm(data.body));
+                }
+            });
+        }).on('click', '#back-to-list', loadPostsForEdit)
+        .on('click', '#edit_post', function(e) {
+            let target = $(e.target);
+            let fd = new FormData();
+            let files = $('#photo_name')[0].files[0];
+            fd.append('image', files);
+            $.ajax({
+                url: backend + '/file/upload',
+                type: 'POST',
+                data: fd,
+                contentType: false,
+                processData: false,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader ("Authorization", "Bearer " + token);
+                }
+            }).done(function(data) {
+                if (data.success) {
+                    let id = $(target).data('id');
+                    if (id) {
+                        $.ajax({
+                            url: backend + '/post/' + $(target).data('id'),
+                            type: 'PUT',
+                            data: JSON.stringify({
+                                "title": $('input#title').val(),
+                                "content": $('textarea#content').val(),
+                                "photo_name": data.body[0]
+                            }),
+                            contentType: "application/json",
+                            beforeSend: function (xhr) {
+                                xhr.setRequestHeader("Authorization", "Bearer " + token);
+                            }
+                        }).done(function (data) {
+                            if (data.success) {
+                                loadPostsForEdit();
+                            }
+                        });
+                    } else {
+                        $.ajax({
+                            url: backend + '/posts',
+                            type: 'POST',
+                            data: JSON.stringify({
+                                "title": $('input#title').val(),
+                                "content": $('textarea#content').val(),
+                                "photo_name": data.body[0]
+                            }),
+                            contentType: "application/json",
+                            beforeSend: function (xhr) {
+                                xhr.setRequestHeader("Authorization", "Bearer " + token);
+                            }
+                        }).done(function (data) {
+                            if (data.success) {
+                                loadPostsForEdit();
+                            }
+                        });
+                    }
+                }
+            });
+            return false;
+        }).on('click', '#add-new', function() {
+            container.html(editForm({"title":"", "content":"", "photo_name":""}));
+        }).on('click', '.fa-trash-alt', function(e) {
+            let id = $(e.target).data('key');
+            $.ajax({
+                url: backend + '/post/' + id,
+                type: 'DELETE',
+                contentType: "application/json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + token);
+                }
+            }).done(function (data) {
+                if (data.success) {
+                    loadPostsForEdit();
+                }
+            });
         });
-    });
-    let loadPostsForEdit = function() {
-        $.get('http://localhost:8080/posts', function (data) {
-            container.html(postAdmin(data));
-        });
-    };
+
 });
